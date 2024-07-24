2b45b957f1d927593de79a2f5607ba64298ecc30
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-24 16:05:53 +0900

    Feat : 애플로그인 로직 수정

--- Cookiee/Cookiee/LoginView.swift
+++ Cookiee/Cookiee/LoginView.swift
@@ -19,31 +19,58 @@ struct LoginView: View {
 
 // * 애플 로그인 버튼
 struct AppleSignInButton : View {
+    @AppStorage("email") var email:String = ""
+    @AppStorage("firstName") var firstName:String = ""
+    @AppStorage("lastName") var lastName:String = ""
+    @AppStorage("userId") var userId:String = ""
+    
     var body: some View {
-        SignInWithAppleButton(onRequest: { request in
-            request.requestedScopes = [.fullName, .email]
-        }, onCompletion: {result in
-            switch result {
-            case .success(let authResults):
-                print("Apple Login Successful")
-                switch authResults.credential{
-                    case let appleIDCredential as ASAuthorizationAppleIDCredential:
-                       // 계정 정보 가져오기
-                        let UserIdentifier = appleIDCredential.user
-                        let fullName = appleIDCredential.fullName
-                        let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
-                        let email = appleIDCredential.email
-                        let IdentityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
-                        let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
-                default:
-                    break
+        if userId.isEmpty {
+            // 애플로 로그인 하기
+            SignInWithAppleButton(.continue) { request in
+                request.requestedScopes = [.email, .fullName]
+                
+            } onCompletion: { result in
+                switch result {
+                case .success(let auth):
+                    switch auth.credential {
+                    case let credential as ASAuthorizationAppleIDCredential:
+                        // User ID
+                        let userId = credential.user
+                        
+                        // User Info
+                        let email = credential.email
+                        let firstName = credential.fullName?.givenName
+                        let lastName = credential.fullName?.familyName
+                        
+                        self.email = email ?? ""
+                        self.userId = userId
+                        self.firstName = firstName ?? ""
+                        self.lastName = lastName ?? ""
+                        
+                        
+                    default:
+                        break
+                    }
+                case .failure(let error):
+                    print(error)
                 }
-            case .failure(let error):
-                print(error.localizedDescription)
-                print("error")
             }
-        })
-        .frame(width : UIScreen.main.bounds.width * 0.7, height:45)
+            .frame(height: 50)
+            .cornerRadius(8)
+        }
+        else {
+            // 로그인 되었을 때
+           VStack {
+               Text("User ID: \(userId)")
+               Text("Email: \(email)")
+               Text("First Name: \(firstName)")
+               Text("Last Name: \(lastName)")
+           }
+           .padding()
+        }
+        
+        
     }
 }
 
