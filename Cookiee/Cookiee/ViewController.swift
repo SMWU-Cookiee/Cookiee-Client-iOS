2db235901dc5128e560690cfaffdd5877abaa6e0
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-07-31 02:19:47 +0900

    Feat : Storyboard 프로젝트 생성

---
+++ Cookiee/Cookiee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 7/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
