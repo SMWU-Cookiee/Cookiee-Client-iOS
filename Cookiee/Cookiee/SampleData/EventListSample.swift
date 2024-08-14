2f11cc8d3378e34daf497707847ec84f81dfba19
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-14 23:41:10 +0900

    Feat : 이벤트 카드 뷰 구현

---
+++ Cookiee/Cookiee/SampleData/EventListSample.swift
@@ -0,0 +1,127 @@
+//
+//  EventListSample.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 8/14/24.
+//
+
+let EventDataSample: [String: Any] = [
+    "isSuccess": true,
+    "statusCode": 1000,
+    "message": "이벤트 조회에 성공하였습니다.",
+    "result": [
+        [
+            "eventId": 3,
+            "what": "초코라떼를 먹었다.",
+            "eventWhere": "잠실에서",
+            "withWho": "황수연이랑",
+            "EventYear": 2023,
+            "EventMonth": 12,
+            "EventDate": 24,
+            "startTime": nil,
+            "endTime": nil,
+            "imageUrlList": [
+                "https://picsum.photos/200/300",
+                "https://picsum.photos/200/300",
+                "https://picsum.photos/200/300"
+            ],
+            "categories": [
+                [
+                    "categoryId": 3,
+                    "categoryName": "여행",
+                    "categoryColor": "#f75eff"
+                ],
+                [
+                    "categoryId": 4,
+                    "categoryName": "동기들",
+                    "categoryColor": "#a3ffb6"
+                ]
+            ]
+        ],
+        [
+            "eventId": 58,
+            "what": "1/30 수정 테스트",
+            "eventWhere": "잠실에서",
+            "withWho": "황수연이랑",
+            "EventYear": 2023,
+            "EventMonth": 12,
+            "EventDate": 24,
+            "startTime": "13:00",
+            "endTime": "18:00",
+            "imageUrlList": [
+                "https://picsum.photos/200/300"
+            ],
+            "categories": [
+                [
+                    "categoryId": 4,
+                    "categoryName": "동기들",
+                    "categoryColor": "#a3ffb6"
+                ]
+            ]
+        ],
+        [
+            "eventId": 58,
+            "what": "1/30 수정 테스트",
+            "eventWhere": "잠실에서",
+            "withWho": "황수연이랑",
+            "EventYear": 2023,
+            "EventMonth": 12,
+            "EventDate": 24,
+            "startTime": "13:00",
+            "endTime": "18:00",
+            "imageUrlList": [
+                "https://picsum.photos/200/300"
+            ],
+            "categories": [
+                [
+                    "categoryId": 4,
+                    "categoryName": "동기들",
+                    "categoryColor": "#a3ffb6"
+                ]
+            ]
+        ],
+        [
+            "eventId": 58,
+            "what": "1/30 수정 테스트",
+            "eventWhere": "잠실에서",
+            "withWho": "황수연이랑",
+            "EventYear": 2023,
+            "EventMonth": 12,
+            "EventDate": 24,
+            "startTime": "13:00",
+            "endTime": "18:00",
+            "imageUrlList": [
+                "https://picsum.photos/200/300"
+            ],
+            "categories": [
+                [
+                    "categoryId": 4,
+                    "categoryName": "동기들",
+                    "categoryColor": "#a3ffb6"
+                ]
+            ]
+        ],
+        [
+            "eventId": 58,
+            "what": "1/30 수정 테스트",
+            "eventWhere": "잠실에서",
+            "withWho": "황수연이랑",
+            "EventYear": 2023,
+            "EventMonth": 12,
+            "EventDate": 24,
+            "startTime": "13:00",
+            "endTime": "18:00",
+            "imageUrlList": [
+                "https://picsum.photos/200/300"
+            ],
+            "categories": [
+                [
+                    "categoryId": 4,
+                    "categoryName": "동기들",
+                    "categoryColor": "#a3ffb6"
+                ]
+            ]
+        ]
+    ]
+]
+
