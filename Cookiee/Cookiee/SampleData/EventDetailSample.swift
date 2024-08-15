46262e6a2486f0020aad6e31f801b069c0a146c2
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-15 23:39:49 +0900

    Feat : 샘플 데이터 Decoder로 수정

---
+++ Cookiee/Cookiee/SampleData/EventDetailSample.swift
@@ -0,0 +1,85 @@
+//
+//  EventDetailSample.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 8/15/24.
+//
+
+import SwiftUI
+
+struct EventCategory: Codable {
+    let categoryId: Int
+    let categoryName: String
+    let categoryColor: String
+}
+
+struct EventResult: Codable {
+    let eventId: Int
+    let what: String
+    let eventWhere: String
+    let withWho: String
+    let eventYear: Int
+    let eventMonth: Int
+    let eventDate: Int
+    let imageUrlList: [String]
+    let categories: [EventCategory]
+}
+
+struct EventData: Codable {
+    let isSuccess: Bool
+    let statusCode: Int
+    let message: String
+    let result: EventResult
+}
+
+class EventViewModel: ObservableObject {
+    @Published var eventData: EventData?
+    
+    init() {
+        self.loadEventData()
+    }
+    
+    func loadEventData() {
+        let jsonData = """
+        {
+            "isSuccess": true,
+            "statusCode": 1000,
+            "message": "이벤트 조회에 성공하였습니다.",
+            "result": {
+                "eventId": 58,
+                "what": "도파민 넘치는 코타키나발루",
+                "eventWhere": "코타키나발루 마무틱",
+                "withWho": "작.변.시",
+                "eventYear": 2024,
+                "eventMonth": 8,
+                "eventDate": 15,
+                "imageUrlList": [
+                    "https://picsum.photos/200/300",
+                    "https://picsum.photos/300/300",
+                    "https://picsum.photos/300/150"
+                ],
+                "categories": [
+                    {
+                        "categoryId": 3,
+                        "categoryName": "여행",
+                        "categoryColor": "#f75eff"
+                    },
+                    {
+                        "categoryId": 4,
+                        "categoryName": "동기들",
+                        "categoryColor": "#a3ffb6"
+                    }
+                ]
+            }
+        }
+        """.data(using: .utf8)!
+        
+        do {
+            let decodedData = try JSONDecoder().decode(EventData.self, from: jsonData)
+            self.eventData = decodedData
+        } catch {
+            print("이벤트 상세 정보 조회 Decode 실패: \(error)")
+        }
+    }
+}
+
--- Cookiee/Cookiee/SampleData/EventListSample.swift
+++ Cookiee/Cookiee/SampleData/EventListSample.swift
@@ -5,123 +5,129 @@
 //  Created by minseo Kyung on 8/14/24.
 //
 
-let EventDataSample: [String: Any] = [
-    "isSuccess": true,
-    "statusCode": 1000,
-    "message": "이벤트 조회에 성공하였습니다.",
-    "result": [
-        [
-            "eventId": 3,
-            "what": "초코라떼를 먹었다.",
-            "eventWhere": "잠실에서",
-            "withWho": "황수연이랑",
-            "EventYear": 2023,
-            "EventMonth": 12,
-            "EventDate": 24,
-            "startTime": nil,
-            "endTime": nil,
-            "imageUrlList": [
-                "https://picsum.photos/200/300",
-                "https://picsum.photos/200/300",
-                "https://picsum.photos/200/300"
-            ],
-            "categories": [
-                [
-                    "categoryId": 3,
-                    "categoryName": "여행",
-                    "categoryColor": "#f75eff"
-                ],
-                [
-                    "categoryId": 4,
-                    "categoryName": "동기들",
-                    "categoryColor": "#a3ffb6"
-                ]
-            ]
-        ],
-        [
-            "eventId": 58,
-            "what": "1/30 수정 테스트",
-            "eventWhere": "잠실에서",
-            "withWho": "황수연이랑",
-            "EventYear": 2023,
-            "EventMonth": 12,
-            "EventDate": 24,
-            "startTime": "13:00",
-            "endTime": "18:00",
-            "imageUrlList": [
-                "https://picsum.photos/200/300"
-            ],
-            "categories": [
-                [
-                    "categoryId": 4,
-                    "categoryName": "동기들",
-                    "categoryColor": "#a3ffb6"
-                ]
-            ]
-        ],
-        [
-            "eventId": 58,
-            "what": "1/30 수정 테스트",
-            "eventWhere": "잠실에서",
-            "withWho": "황수연이랑",
-            "EventYear": 2023,
-            "EventMonth": 12,
-            "EventDate": 24,
-            "startTime": "13:00",
-            "endTime": "18:00",
-            "imageUrlList": [
-                "https://picsum.photos/200/300"
-            ],
-            "categories": [
-                [
-                    "categoryId": 4,
-                    "categoryName": "동기들",
-                    "categoryColor": "#a3ffb6"
-                ]
-            ]
-        ],
-        [
-            "eventId": 58,
-            "what": "1/30 수정 테스트",
-            "eventWhere": "잠실에서",
-            "withWho": "황수연이랑",
-            "EventYear": 2023,
-            "EventMonth": 12,
-            "EventDate": 24,
-            "startTime": "13:00",
-            "endTime": "18:00",
-            "imageUrlList": [
-                "https://picsum.photos/200/300"
-            ],
-            "categories": [
-                [
-                    "categoryId": 4,
-                    "categoryName": "동기들",
-                    "categoryColor": "#a3ffb6"
-                ]
-            ]
-        ],
-        [
-            "eventId": 58,
-            "what": "1/30 수정 테스트",
-            "eventWhere": "잠실에서",
-            "withWho": "황수연이랑",
-            "EventYear": 2023,
-            "EventMonth": 12,
-            "EventDate": 24,
-            "startTime": "13:00",
-            "endTime": "18:00",
-            "imageUrlList": [
-                "https://picsum.photos/200/300"
-            ],
-            "categories": [
-                [
-                    "categoryId": 4,
-                    "categoryName": "동기들",
-                    "categoryColor": "#a3ffb6"
-                ]
-            ]
-        ]
-    ]
-]
+import Foundation
 
+
+struct EventResponse: Codable {
+    let isSuccess: Bool
+    let statusCode: Int
+    let message: String
+    let result: [Event]
+}
+
+struct Event: Codable, Identifiable {
+    let id: Int
+    let what: String
+    let eventWhere: String
+    let withWho: String
+    let EventYear: Int
+    let EventMonth: Int
+    let EventDate: Int
+    let startTime: String?
+    let endTime: String?
+    let imageUrlList: [String]
+    let categories: [Category]
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "eventId"
+        case what
+        case eventWhere
+        case withWho
+        case EventYear
+        case EventMonth
+        case EventDate
+        case startTime
+        case endTime
+        case imageUrlList
+        case categories
+    }
+}
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+    let color: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "categoryId"
+        case name = "categoryName"
+        case color = "categoryColor"
+    }
+}
+
+
+class EventListViewModel: ObservableObject {
+    @Published var eventListData: EventResponse?
+
+    init() {
+        self.loadEventListData()
+    }
+
+    func loadEventListData() {
+        let jsonData = """
+        {
+            "isSuccess": true,
+            "statusCode": 1000,
+            "message": "이벤트 조회에 성공하였습니다.",
+            "result": [
+                {
+                    "eventId": 3,
+                    "what": "초코라떼를 먹었다.",
+                    "eventWhere": "잠실에서",
+                    "withWho": "황수연이랑",
+                    "EventYear": 2023,
+                    "EventMonth": 12,
+                    "EventDate": 24,
+                    "startTime": null,
+                    "endTime": null,
+                    "imageUrlList": [
+                        "https://picsum.photos/200/300",
+                        "https://picsum.photos/200/300",
+                        "https://picsum.photos/200/300"
+                    ],
+                    "categories": [
+                        {
+                            "categoryId": 3,
+                            "categoryName": "여행",
+                            "categoryColor": "#f75eff"
+                        },
+                        {
+                            "categoryId": 4,
+                            "categoryName": "동기들",
+                            "categoryColor": "#a3ffb6"
+                        }
+                    ]
+                },
+                {
+                    "eventId": 58,
+                    "what": "1/30 수정 테스트",
+                    "eventWhere": "잠실에서",
+                    "withWho": "황수연이랑",
+                    "EventYear": 2023,
+                    "EventMonth": 12,
+                    "EventDate": 24,
+                    "startTime": "13:00",
+                    "endTime": "18:00",
+                    "imageUrlList": [
+                        "https://picsum.photos/200/300"
+                    ],
+                    "categories": [
+                        {
+                            "categoryId": 4,
+                            "categoryName": "동기들",
+                            "categoryColor": "#a3ffb6"
+                        }
+                    ]
+                }
+            ]
+        }
+        """.data(using: .utf8)!
+
+        do {
+            let decodedData = try JSONDecoder().decode(EventResponse.self, from: jsonData)
+            self.eventListData = decodedData
+        } catch {
+            print("이벤트 리스트 조회 Decode 실패: \(error)")
+        }
+    }
+}
