f027eb7728eef92ec6baff88809ea05269fe6c4f
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-24 16:00:19 +0900

    Feat : 추가하기 버튼 구현

--- Cookiee/Cookiee/CategoryEditView.swift
+++ Cookiee/Cookiee/CategoryEditView.swift
@@ -11,6 +11,7 @@ struct CategoryEditView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @StateObject private var viewModel = CategoryListViewModel()
     @State var isModalOpen: Bool = false
+    @State private var isAddButtonTapped = false
 
     // 백 버튼 커스텀
     var backButton: some View {
@@ -30,7 +31,11 @@ struct CategoryEditView: View {
                 ForEach(viewModel.categoryListData?.result ?? []) { category in
                     CategoryListView(name: category.name, color: category.color)
                 }
+                CategoryAddButtonView(toggleIsTapped: {
+                    isAddButtonTapped.toggle()
+                })
             }
+            
         }
         .navigationBarTitle(
             Text("카테고리 관리")
@@ -84,6 +89,41 @@ struct CategoryListView: View {
                 })
             }
             Divider()
+        }
+    }
+}
+
+struct CategoryAddButtonView: View {
+    @State var name: String = ""
+    @FocusState private var isTextFieldFocused: Bool
+    @State var toggleIsTapped: () -> Void
+    
+    var body: some View {
+        VStack {
+            HStack {
+                // 색상
+                Rectangle()
+                    .fill(Color.Gray02)
+                    .frame(width: 25, height: 25)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .overlay(Image("Plus"))
+                
+                // 이름
+                HStack {
+                    Button(action: {
+                        toggleIsTapped()
+                    }, label: {
+                        Text("추가하기")
+                            .font(.Body1_M)
+                            .foregroundColor(.black)
+                    })
+                    
+                }
+                .font(.Body1_M)
+                .frame(height: 35)
+                
+                Spacer()
+            }
         }
     }
 }
