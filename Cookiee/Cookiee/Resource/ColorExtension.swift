76c281f97777ad7bba9feeea28851f2abd12b17e
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-25 23:44:42 +0900

    Feat : UIColor -> hex 변환 extension 작성

--- Cookiee/Cookiee/Resource/ColorExtension.swift
+++ Cookiee/Cookiee/Resource/ColorExtension.swift
@@ -42,3 +42,19 @@ extension Color {
     self.init(red: r, green: g, blue: b)
     }
 }
+
+// UIColor -> hex
+extension UIColor {
+    func toHexString() -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
+        
+        return String(format: "#%06x", rgb)
+    }
+}
