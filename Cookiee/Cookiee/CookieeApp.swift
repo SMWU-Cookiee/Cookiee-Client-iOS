84243faef2fd1450f2b7e21e193ae6e44cb4abe4
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-09-25 23:32:07 +0900

    Feat : 구글로그인 핸들러 CookieeCustomDelegate 작성

--- Cookiee/Cookiee/CookieeApp.swift
+++ Cookiee/Cookiee/CookieeApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import GoogleSignInSwift
 
 @main
 struct CookieeApp: App {
+    @UIApplicationDelegateAdaptor var delegate: CookieeCustomDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class CookieeCustomDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
