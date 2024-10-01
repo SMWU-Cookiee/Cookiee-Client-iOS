7a7f866fa37a775e6bd49ed8e0d014cc694d1947
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-01 20:19:29 +0900

    Feat : Account Group 생성

---
+++ Cookiee/Cookiee/Presentation/Account/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 10/1/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
