7fd53d54fe536b4dfa2461bcd9b1cc4503a546c9
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-05 00:26:30 +0900

    Feat : TokenInterceptor에 shared 객체 추가

--- Cookiee/Cookiee/Network/Auth/Token/TokenInterceptor.swift
+++ Cookiee/Cookiee/Network/Auth/Token/TokenInterceptor.swift
@@ -10,6 +10,10 @@ import Alamofire
 
 final class TokenInterceptor: RequestInterceptor {
     
+    static let shared = TokenInterceptor()
+
+    private init() {}
+    
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("retry 진입")
         guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401
