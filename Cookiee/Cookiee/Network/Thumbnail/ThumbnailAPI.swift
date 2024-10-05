b0684af40c2ef68b08f831c8e5f046959f524db3
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-05 14:13:23 +0900

    Feat : ThumbnailAPI에 getThumbnailList() 작성

---
+++ Cookiee/Cookiee/Network/Thumbnail/ThumbnailAPI.swift
@@ -0,0 +1,42 @@
+//
+//  ThumbnailAPI.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 10/4/24.
+//
+
+import Foundation
+import Moya
+
+enum ThumbnailAPI {
+    case getThumbnailList(userId: String)
+}
+
+extension ThumbnailAPI: BaseTargetType {
+    var headerType: HeaderType {
+        .accessTokenHeaderForGet
+    }
+    
+    var path: String {
+        switch self {
+        case .getThumbnailList(userId: let userId):
+            return "/api/v2/thumbnails/\(userId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getThumbnailList:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getThumbnailList(_):
+            return .requestPlain
+        }
+    }
+    
+    
+}
