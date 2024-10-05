21934a9332ab775393f2246b76ae9414cfe717b0
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-05 14:13:06 +0900

    Feat : ThumbnailListResponseDTO 작성

---
+++ Cookiee/Cookiee/Network/Thumbnail/DTO/Response/ThumbnailListResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  ThumbnailResponseDTO.swift
+//  Cookiee
+//
+//  Created by minseo Kyung on 10/4/24.
+//
+
+import Foundation
+
+struct ThumbnailListResponseDTO : Codable {
+    let isSuccess: Bool
+    let statusCode: Int
+    let message: String
+    let result: [ThumbnailResultData]
+}
+
+struct ThumbnailResultData: Codable {
+    let thumbnailId: Int64
+    let eventYear: Int32
+    let eventMonth: Int32
+    let eventDate: Int32
+    let thumbnailUrl: String
+}
