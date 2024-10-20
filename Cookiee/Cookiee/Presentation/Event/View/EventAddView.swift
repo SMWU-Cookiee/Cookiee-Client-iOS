483c7373dbca53093b6c572d39dd551b564bf1e6
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-20 17:40:19 +0900

    Fix : 사진 선택 영역까지 ScrollView로 연결

--- Cookiee/Cookiee/Presentation/Event/View/EventAddView.swift
+++ Cookiee/Cookiee/Presentation/Event/View/EventAddView.swift
@@ -30,44 +30,40 @@ struct EventAddView: View {
     
    
     var body: some View {
-        VStack {
+        ScrollView {
             InitialAddMessageCardView()
                 .padding(.bottom, 14)
             
-            ScrollView {
-                VStack {
-                    EventInfoTextField(fieldName: "쿠키 제목", placeholder: "쿠키의 제목을 입력해주세요.", field: title)
-                    EventInfoTextField(fieldName: "장소", placeholder: "장소를 입력해주세요.", field: place)
-                    EventInfoTextField(fieldName: "내용", placeholder: "어떤 활동을 하셨나요?", field: content)
-                    EventInfoTextField(fieldName: "함께한 사람", placeholder: "함께한 사람들을 입력해주세요.", field: people)
-                }
-                .padding(.bottom, 15)
-                
-                VStack(alignment: .leading) {
-                    Text("카테고리")
-                        .font(.Body1_M)
-                        .foregroundStyle(Color.Gray05)
-                        .frame(width: 75, alignment: .leading)
-                    Button(action: {
-                        
-                    }) {
-                        HStack {
-                            Text("카테고리를 선택해주세요.")
-                                .font(.Body0_M)
-                                .foregroundStyle(Color.Black)
-                            Spacer()
-                            Image("UnderDropBlack")
-                                .padding(2)
-                        }
-                        .padding(10)
-                        .frame(height: 40)
-                        .background(Color.Gray01)
-                        .cornerRadius(5)
-                    }
-                }
-                .padding(.bottom, 25)
+            VStack {
+                EventInfoTextField(fieldName: "쿠키 제목", placeholder: "쿠키의 제목을 입력해주세요.", field: title)
+                EventInfoTextField(fieldName: "장소", placeholder: "장소를 입력해주세요.", field: place)
+                EventInfoTextField(fieldName: "내용", placeholder: "어떤 활동을 하셨나요?", field: content)
+                EventInfoTextField(fieldName: "함께한 사람", placeholder: "함께한 사람들을 입력해주세요.", field: people)
             }
-            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("카테고리")
+                    .font(.Body1_M)
+                    .foregroundStyle(Color.Gray05)
+                    .frame(width: 75, alignment: .leading)
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Text("카테고리를 선택해주세요.")
+                            .font(.Body0_M)
+                            .foregroundStyle(Color.Black)
+                        Spacer()
+                        Image("UnderDropBlack")
+                            .padding(2)
+                    }
+                    .padding(10)
+                    .frame(height: 40)
+                    .background(Color.Gray01)
+                    .cornerRadius(5)
+                }
+            }
+            .padding(.bottom, 25)
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
