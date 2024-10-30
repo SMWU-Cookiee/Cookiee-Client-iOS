28e7a6eb24f6e8953c6e0ee7d74dd054e8d4dde4
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-10-31 01:17:32 +0900

    Feat : 카테고리 리스트 뷰 구현

--- Cookiee/Cookiee/Presentation/CookieeCollection/View/CookieeCollectionView.swift
+++ Cookiee/Cookiee/Presentation/CookieeCollection/View/CookieeCollectionView.swift
@@ -9,12 +9,58 @@ import SwiftUI
 
 struct CookieeCollectionView: View {
     var body: some View {
-        Spacer()
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("쿠키 모아보기")
+            .font(Font.Head1_B)
+            .foregroundStyle(Color.Brown00)
+            .frame(height: 44)
+        
+        VStack(spacing: 0) {
+            CategoryListRowViewForCollection(id: "1", name: "#343434", color: "343434", isCollectionExist: true)
+            CategoryListRowViewForCollection(id: "1", name: "#343434", color: "343434", isCollectionExist: true)
+        }
+
         Spacer()
     }
 }
 
 #Preview {
     CookieeCollectionView()
+}
+
+// MARK: - 카테고리 리스트 (수정 버튼, 삭제 버튼)
+struct CategoryListRowViewForCollection: View {
+    @State var id: String = ""
+    @State var name: String = ""
+    @State var color: String
+    @State var isCollectionExist: Bool
+    
+        
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Rectangle()
+                    .fill(Color(hex: color))
+                    .frame(width: 25, height: 25)
+                    .cornerRadius(5.0)
+                    .padding(.trailing, 20)
+                
+                HStack {
+                    Text(name)
+                        .font(.Body0_M)
+                        .foregroundStyle(Color.Gray07)
+                    Spacer()
+                }
+                
+                if isCollectionExist {
+                    Image("CookieeIcon_fill")
+
+                }
+            }
+            .frame(height: 48)
+            
+            Divider()
+                .foregroundStyle(Color.Gray01)
+        }
+        .padding(.horizontal, 20)
+    }
 }
