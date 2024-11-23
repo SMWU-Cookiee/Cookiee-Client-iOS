3b227ffd17ed9dc15e0b23743453129ebaf62c20
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-23 17:47:18 +0900

    Fix : 이벤트 추가 이미지 중앙정렬

--- Cookiee/Cookiee/Presentation/Event/View/EventAddView/ImageCarousel.swift
+++ Cookiee/Cookiee/Presentation/Event/View/EventAddView/ImageCarousel.swift
@@ -11,7 +11,7 @@ import PhotosUI
 struct ImageCarouselForPhotoPicker: View {
     @ObservedObject var viewModel: ImagePickerForEventViewModel
 
-    var spacing: CGFloat = 10
+    var spacing: CGFloat = 15
     var trialingSpace: CGFloat = 30
     
     @GestureState var offset: CGFloat = 0
