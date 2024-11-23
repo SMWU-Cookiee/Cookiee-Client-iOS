ef7399dbe9cbbfb32215063c98c893db0100064e
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-23 17:40:10 +0900

    Fix : ImageCarouselView dragGesture modifier 분리

--- Cookiee/Cookiee/UIComponent/ImageCarouselView.swift
+++ Cookiee/Cookiee/UIComponent/ImageCarouselView.swift
@@ -22,45 +22,26 @@ struct ImageCarouselView: View {
         self.trialingSpace = trialingSpace
         self._index = index
     }
-    
     var body: some View {
-        VStack(alignment: .center) {
-            GeometryReader { proxy in
-                let width = proxy.size.width - (trialingSpace - spacing)
-                let adjustmentWidth = (trialingSpace / 2) - spacing
-                
-                HStack(alignment: .center, spacing: spacing) {
-                    ForEach(Array(imageUrls.enumerated()), id: \.offset) { offset, url in
-                        fetchImageByURL(url: url)
-                        .frame(width: proxy.size.width - trialingSpace)
+        VStack {
+            VStack {
+                GeometryReader { proxy in
+                    let width = proxy.size.width - (trialingSpace - spacing)
+                    let adjustmentWidth = (trialingSpace / 2) - spacing
+                    
+                    HStack(spacing: spacing) {
+                        ForEach(Array(imageUrls.enumerated()), id: \.offset) { offset, url in
+                            fetchImageByURL(url: url)
+                            .frame(width: proxy.size.width - trialingSpace)
+                        }
                     }
+                    .padding(.horizontal, spacing)
+                    .offset(x: (CGFloat(currentIndex) * -width) + (currentIndex != 0 ? adjustmentWidth : 0) + offset)
+                    .gesture(dragGesture(width: width))
                 }
-                .padding(.horizontal, spacing)
-                .offset(x: (CGFloat(currentIndex) * -width) + (currentIndex != 0 ? adjustmentWidth : 0) + offset)
-                .gesture(
-                    DragGesture()
-                        .updating($offset, body: { value, out, _ in
-                            out = (value.translation.width / 1.5)
-                        })
-                        .onEnded { value in
-                            let offsetX = value.translation.width
-                            let progress = -offsetX / width
-                            let roundIndex = progress.rounded()
-                            
-                            currentIndex = max(min(currentIndex + Int(roundIndex), imageUrls.count - 1), 0)
-                            
-                            index = currentIndex
-                        }
-                        .onChanged { value in
-                            let offsetX = value.translation.width
-                            let progress = -offsetX / width
-                            let roundIndex = progress.rounded()
-                            
-                            index = max(min(currentIndex + Int(roundIndex), imageUrls.count - 1), 0)
-                        }
-                )
+                .animation(.easeInOut, value: offset == 0)
             }
-            .animation(.easeInOut, value: offset == 0)
+            .frame(height: 360)
             
             HStack(spacing: 8) {
                 ForEach(0..<imageUrls.count, id: \.self) { i in
@@ -77,6 +58,29 @@ struct ImageCarouselView: View {
                 }
             }
         }
+    }
+    
+    private func dragGesture(width: CGFloat) -> some Gesture {
+        DragGesture()
+            .updating($offset, body: { value, out, _ in
+                out = (value.translation.width / 1.5)
+            })
+            .onEnded { value in
+                let offsetX = value.translation.width
+                let progress = -offsetX / width
+                let roundIndex = progress.rounded()
+                
+                currentIndex = max(min(currentIndex + Int(roundIndex), imageUrls.count - 1), 0)
+                
+                index = currentIndex
+            }
+            .onChanged { value in
+                let offsetX = value.translation.width
+                let progress = -offsetX / width
+                let roundIndex = progress.rounded()
+                
+                index = max(min(currentIndex + Int(roundIndex), imageUrls.count - 1), 0)
+            }
     }
 }
 
