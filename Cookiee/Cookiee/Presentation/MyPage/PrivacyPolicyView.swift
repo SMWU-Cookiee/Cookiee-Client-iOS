6c1c20de4068ce664bcc576c42b98a32ad5ff00a
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-12-17 13:58:14 +0900

    Feat : 이용약관 및 개인정보 활용방침 인앱 사파리로 연결

--- Cookiee/Cookiee/Presentation/MyPage/PrivacyPolicyView.swift
+++ Cookiee/Cookiee/Presentation/MyPage/PrivacyPolicyView.swift
@@ -21,29 +21,36 @@ struct PrivacyPolicyView: View {
         }
     }
     
+    @State var showTermsOfService = false
+    @State var showPrivacyPolicy = false
+    
     var body: some View {
         VStack {
             HStack {
-                Link(destination: URL(string: "https://thunder-syrup-94d.notion.site/6a378d5d676444cbad61bbe971591da5?pvs=74")!) {
+                Button(action: {
+                    showTermsOfService = true
+                }, label: {
                     Text("이용약관")
                         .font(.Body1_M)
                         .foregroundStyle(Color.black)
                     Spacer()
                     Image("ChevronRightSmall")
                         .padding(.horizontal, 10)
-                }
+                })
                 .frame(height: 32)
             }
             Divider()
             HStack {
-                Link(destination: URL(string: "https://thunder-syrup-94d.notion.site/cc6dbac1d4ee4a05b7555e4fd398f4a1?pvs=74")!) {
+                Button(action: {
+                    showPrivacyPolicy = true
+                }, label: {
                     Text("개인정보 활용방침")
                         .font(.Body1_M)
                         .foregroundStyle(Color.black)
                     Spacer()
                     Image("ChevronRightSmall")
                         .padding(.horizontal, 10)
-                }
+                })
                 .frame(height: 32)
             }
             Spacer()
@@ -58,6 +65,14 @@ struct PrivacyPolicyView: View {
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: backButton)
         .padding()
+        
+        .sheet(isPresented: $showTermsOfService) {
+            SafariView(url:URL(string: "https://thunder-syrup-94d.notion.site/6a378d5d676444cbad61bbe971591da5?pvs=74")!)
+        }
+        
+        .sheet(isPresented: $showPrivacyPolicy) {
+            SafariView(url:URL(string: "https://thunder-syrup-94d.notion.site/cc6dbac1d4ee4a05b7555e4fd398f4a1")!)
+        }
         
     }
 }
