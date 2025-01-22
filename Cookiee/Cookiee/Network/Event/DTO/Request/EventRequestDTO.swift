e96114f18bc28b2c19c466a319a401604a5e68ab
mxinseo <110973127+mxinseo@users.noreply.github.com>
2025-01-22 22:48:15 +0900

    Feat : 이벤트 DTO에 지도 정보 추가

--- Cookiee/Cookiee/Network/Event/DTO/Request/EventRequestDTO.swift
+++ Cookiee/Cookiee/Network/Event/DTO/Request/EventRequestDTO.swift
@@ -10,11 +10,19 @@ import Foundation
 public struct EventRequestDTO: Codable {
     let eventTitle: String
     let eventWhat: String
-    let eventWhere: String
+    let eventWhereText: String?
+    let eventWherePlace: EventWherePlace?
     let withWho: String
     let eventYear: Int32
     let eventMonth: Int32
     let eventDate: Int32
     let categoryIds: [Int64]
     let images: [Data]
+}
+
+struct EventWherePlace: Codable {
+    let latitude: String
+    let longitude: String
+    let name: String
+    let fullAddress: String
 }
