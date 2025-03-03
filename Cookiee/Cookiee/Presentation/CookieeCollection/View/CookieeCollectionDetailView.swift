22eb23a7612627cd0c5585edb33695542af37304
mxinseo <kyungminseo10@gmail.com>
2025-03-03 14:36:52 +0900

    Feat : 쿠키 모아보기에 이벤트 상세 조회 페이지 연결

--- Cookiee/Cookiee/Presentation/CookieeCollection/View/CookieeCollectionDetailView.swift
+++ Cookiee/Cookiee/Presentation/CookieeCollection/View/CookieeCollectionDetailView.swift
@@ -21,6 +21,8 @@ struct CookieeCollectionDetailView: View {
     }
     
     @ObservedObject var cookieeCollectionViewModel = CookieeCollectionViewModel()
+    @ObservedObject var eventViewModel = EventViewModel()
+    @State private var isEventDetailViewModalOpen: Bool = false
     
     var id: Int64
     private let columns = [
@@ -58,16 +60,7 @@ struct CookieeCollectionDetailView: View {
                         ScrollView {
                             LazyVGrid(columns: columns, spacing: 1) {
                                 ForEach(cookieeCollectionDetail.eventImageList, id: \.eventId) { eventImage in
-                                    AsyncImage(url: URL(string: eventImage.firstImageUrl)) { image in
-                                        image
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                            .frame(width: cellWidth, height: cellWidth)
-                                            .clipped()
-                                    } placeholder: {
-                                        ProgressView()
-                                            .frame(width: cellWidth, height: cellWidth)
-                                    }
+                                    ThumbnailButtonToEventDetail(url: eventImage.firstImageUrl, cellWidth: cellWidth, eventId: eventImage.eventId)
                                 }
                             }
                             .padding(.horizontal, 1)
@@ -94,6 +87,32 @@ struct CookieeCollectionDetailView: View {
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: backButton)
+        .sheet(isPresented: $isEventDetailViewModalOpen) {
+            if eventViewModel.selectedEventId != nil {
+                EventDetailView(eventViewModel: eventViewModel)
+                    .presentationDetents([.fraction(0.99)])
+                    .presentationDragIndicator(Visibility.visible)
+            }
+        }
+    }
+    
+    private func ThumbnailButtonToEventDetail(url: String, cellWidth: CGFloat, eventId: Int64) -> some View {
+        Button(action: {
+            print(eventId)
+            eventViewModel.selectedEventId = eventId
+            isEventDetailViewModalOpen = true
+        }, label: {
+            AsyncImage(url: URL(string: url)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: cellWidth, height: cellWidth)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
+                    .frame(width: cellWidth, height: cellWidth)
+            }
+        })
     }
 }
 
